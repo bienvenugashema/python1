94868adee71f3e50ec0f8ebc5476f2b7a51bc181
MWIMULE Bienvenu <bienvenugashema@gmail.com>
2025-05-27 13:52:46 +0400

    adding oop

---
+++ oop/answer.py
@@ -0,0 +1,20 @@
+class Book:
+    books = dict()
+    def __init__(this, title, author, pages:int):
+        this.title = title
+        this.author = author
+        this.pages = pages
+
+    @classmethod
+    def add_book(cls, title, author, pages):
+        book = dict()
+        obj = cls(title,author,pages)
+        book['author'] = obj.author
+        book['pages'] = obj.pages
+        cls.books[obj.title] = book
+
+        return cls.books
+
+obj = Book.add_book("Mathematics", "Mwimule Bienvenu", 90)
+print(obj)    
+        
