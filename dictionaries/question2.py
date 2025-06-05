9ed0b4a3cbe088f326f19cf78ce3343f71a1a057
bienvenugashema <bienvenugashema@gmail.com>
2025-06-05 20:19:54 +0200

    Adding dictiinary queations folder

---
+++ dictionaries/question2.py
@@ -0,0 +1,13 @@
+temperatureCountries = {
+    'Rwanda': 29,
+    'Nigeria': 28,
+    'Kenya': 23,
+    'Egypt': 37,
+    'Morocco': 41,
+    'SouthAfrica': 25,
+    'Mali': 33,
+    'Ghana': 28
+}
+
+temperatureCountries['Morocco'] = 38
+print(temperatureCountries)
