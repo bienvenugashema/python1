b0abd7a0bbbaf533739b2cc32bc1f568a12f9fd6
MWIMULE Bienvenu <bienvenugashema@gmail.com>
2025-06-09 18:02:13 +0400

    Adding the sum

--- extended_questions/8/answer.py
+++ extended_questions/8/answer.py
@@ -4,11 +4,15 @@ matrix = [
 ]
 
 N = len(matrix)
-left_diagonal = []
+right_diagonal = []
 start = N - 1
 for i in range(N):
     value = matrix[i][start]
     start -= 1
-    left_diagonal.append(value)
+    right_diagonal.append(value)
 
-print(left_diagonal)
+print(right_diagonal)
+
+# Sum of elements
+
+print(sum(right_diagonal))
