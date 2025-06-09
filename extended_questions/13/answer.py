1af138a704fc1abc34b9560944b2d5d441a71ebd
MWIMULE Bienvenu <bienvenugashema@gmail.com>
2025-06-09 18:44:15 +0400

    Adding question 13

---
+++ extended_questions/13/answer.py
@@ -0,0 +1,15 @@
+def count_ints(lst:list):
+    dic = {'Positive':0, "Negative": 0, "Zeroes": 0}
+    for nums in lst:
+        if nums > 0:
+            dic['Positive'] +=1
+        if nums < 0:
+            dic['Negative'] += 1
+        if nums == 0:
+            dic['Zeroes'] += 1
+    return dic             
+
+
+
+lis = [1,2,3,4,-3,-2,-8,0,1]
+print(count_ints(lis))
